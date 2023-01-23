5a424664c8cfaf8c0ceacde83b0ccb60a65b6644
Matias Cena <31932103+hipermatu@users.noreply.github.com>
2023-01-23 15:21:36 -0300

    Create PriceConsumer.sol

---
+++ PriceConsumer.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "hardhat/console.sol";
+
+contract PriceConsumerV3 {
+    AggregatorV3Interface public priceFeed;
+    constructor() public {
+        priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+    }
+
+    function getLatestPrice() public view returns (int) {
+        (,int price,,,) = priceFeed.latestRoundData();
+        return(price);
+    }
+}
